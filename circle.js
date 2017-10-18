896c7c25a3328c0e3fd09ee00a9788a60a64689c
rocket3989 <rocket3989@gmail.com>
2017-10-18 08:45:57 -0500

    Update circle.js

--- circle.js
+++ circle.js
@@ -12,7 +12,7 @@ function circle(posx,posy,radius,offset,vel,network){
 		this.pointy = sin(this.vel*t+this.offset)*radius+posy;	
 	}
 	this.show = function(){
-		ellipse(this.posx,this.posy,this.radius*2,this.radius*2);
+		//ellipse(this.posx,this.posy,this.radius*2,this.radius*2);
 		for(j = 0;j<this.network.length;j++){
 			line(this.pointx,this.pointy,circles[this.network[j]].pointx,circles[this.network[j]].pointy);
 		}
