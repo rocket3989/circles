bef1b7ae1264caafbbe5bc9a6d4ce44b8ef81957
Collin <rocket3989@gmail.com>
2017-10-17 19:12:22 -0500

    being silly

--- sketch.js
+++ sketch.js
@@ -9,16 +9,17 @@ function setup() {
 	stroke(255);
 	noFill();
 	var network = [];
-	for (i=1;i<25;i++)
+	var length = 10;
+	var width = 10;
+	for (i=1;i<(length*width);i++)
 		network.push(i);
-	for (i = 0;i<5;i++){
-		for (j=0;j<5;j++){
+	for (i = 0;i<length;i++){
+		for (j=0;j<width;j++){
 			circles.push(new circle(50+50*j,50+50*i,20,0,0.01+.003*j-.003*i,network.slice()));
 			network.splice(0,1);
-			
 			}
 		}
-	console.log(circles[24].network);
+	//console.log(circles[24].network);
 }
 function windowResized(){
 	resizeCanvas(windowWidth, windowHeight);
@@ -27,7 +28,7 @@ function windowResized(){
 function draw() {
 	t++;
 	background(0);
-	text(frameRate(),500,500,20,20);
+	//text(frameRate(),500,500,20,20);
 	for (i = 0 ; i < circles.length ; i++){
 		circles[i].findPoint();
 	}
